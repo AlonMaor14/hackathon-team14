ddaf75f548a3361fd8bd7d94cfc07afb70551c48
AlonMaor14 <maoralo@post.bgu.ac.il>
2020-12-26 16:59:56 +0200

    real ip

--- server/server.py
+++ server/server.py
@@ -1,23 +1,59 @@
 import socket
 import time
+import struct
 from threading import Thread
+from scapy.all import *
 
-def send_offer():
-    UDP_IP = "127.0.0.1"
+def send_offer(UDP_IP):
     UDP_PORT = 13117
-    packet = bytearray.fromhex('feedbeef022086')
-    print(f'Server started, listening on IP address {UDP_IP}')
+
+    # prefix = 0xfeedbeef, type = 0x02, port = 2086
+    packet = struct.pack('lhh', 0xfeedbeef, 0x2, 0x2086)
+
+    # send offers for 10 seconds
     start_time = time.time()
+
+    # init socket to address family (host, port) and for UDP connection
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while(time.time() - start_time < 10):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(packet, (UDP_IP, UDP_PORT))
+    
+    sock.close()
+    
+
+def listen_for_clients(TCP_IP):
+    TCP_PORT = 2086
+    BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+    
+    # init socket to address family (host, port) and for TCP connection
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+
+    # we want it to queue up as many as 5 connect requests ?
+    s.listen(5)
+   
+    conn, addr = s.accept()
+    print(f'Connection address:{addr}')
+    data = conn.recv(BUFFER_SIZE)
+    print(f'received data:{data.decode("utf-8")}')
+    conn.send(data)  # echo
+    time.sleep(5)
+    conn.close()
+    return True
 
 def main():
-    thread = Thread(target = send_offer)
-    thread.start()
+    IP = get_if_addr("eth1")
+    print(f'Server started, listening on IP address {IP}')
 
-
-    thread.join()
+    while True:
+        thread = Thread(target = send_offer, args=[IP])
+        thread.start()
+        game = listen_for_clients(IP)
+        thread.join()
+        if game:
+            print('Game over, sending out offer requests...' )
+        else:
+            print('Looking for players...')
 
 
 if __name__ == "__main__":
