f17e42a95a69d8a3b441ba898f93ca5ca70b3c6d
AlonMaor14 <maoralo@post.bgu.ac.il>
2020-12-26 21:13:59 +0200

    added flow

--- server/server.py
+++ server/server.py
@@ -2,7 +2,11 @@ import socket
 import time
 import struct
 from threading import Thread
+import signal
+import sys
 from scapy.all import *
+from concurrent.futures import ThreadPoolExecutor
+
 
 def send_offer(UDP_IP):
     UDP_PORT = 13117
@@ -21,43 +25,107 @@ def send_offer(UDP_IP):
     sock.close()
     
 
-def listen_for_clients(TCP_IP):
+def play(TCP_IP):
     TCP_PORT = 2086
     BUFFER_SIZE = 1024
     teams = {}
+    group1 = []
+    group2 = []
     
     # init socket to address family (host, port) and for TCP connection
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((TCP_IP, TCP_PORT))
 
-    # we want it to queue up as many as 5 connect requests ?
+    # make socket a server's one, 5 is only advisory for num of connections
     s.listen(5)
-   
-    # TODO: add timeout 10 secs and manage players ir return false
-    conn, addr = s.accept()
-    print(f'Connection address:{addr}')
-    data = conn.recv(BUFFER_SIZE)
-    team_name = data.decode("utf-8")
-    print(f'received data:{tean_name}')
-    teams[team_name] = conn
-    conn.send(b'Welcome to Keyboard Spamming Battle Royale.')
 
-    # open threadpool
-    time.sleep(5)
-    conn.close()
+    game_over_msg = 'Some wierd stuff happend\n'
+    try:
+        connect_to_clients(s, teams, group1, group2)
+
+        # if no teams don't start game
+        if bool(teams) :
+            return False
+        
+        for conn in teams.values():
+            conn.send(b'Group 1 :\n==\n{0}\n'.format('\n'.join(group1)))
+            conn.send(b'Group 2 :\n==\n{0}\n'.format('\n'.join(group2)))
+
+        # shutdown automatically
+        with ThreadPoolExecutor() as executor:
+            scores_futures = []
+            for team, conn in teams.items():
+                scores_futures.append(executor.submit(player_runnable, args=[team, conn]))
+                conn.send(b'START SPAMMING!!!!!\n')
+
+            # Main thread computes results
+            g1_res = 0
+            g2_res = 0
+            for res in concurrent.futures.as_completed(scores_futures):
+                res = res.result()
+                if res[0] in group1:
+                    g1_res += res[1]
+                else: 
+                    g2_res += res[1]
+
+        game_over_msg = f'Game Over!\n Group 1 score: {g1_res}\n Group 2 score: {g2_res}\n'
+        if g1_res > g2_res:
+            game_over_msg += 'Winners : Group 1 !\n'
+        elif g2_res > g1_res:
+            game_over_msg += 'Winners : Group 2 !\n'
+        else:
+            game_over_msg += 'Tie !\n'
+
+        
+    finally:
+        for conn in teams.values():
+            conn.send(struct.pack('s', game_over_msg.encode()))
+            conn.close()
+        print('closing socket')
+        s.close()
+    
     return True
 
-def player_runnable(conn):
+def player_runnable(team, conn):
     BUFFER_SIZE = 1
     score = 0
     while True:
         data = conn.recv(BUFFER_SIZE)
         if not data: break
         score += len(data)
-    return score
+    return (team, score)
+
+def connect_to_clients(socket, teams, group1, group2):
+    BUFFER_SIZE = 1024
+
+    # start game after 10 seconds
+    start_time = time.time()
+    group_index = 1
+    if time.time() - start_time < 10:
+        conn, addr = socket.accept()
+        print(f'Connection address:{addr}')
+
+        data = conn.recv(BUFFER_SIZE)
+        team_name = data.decode("utf-8")
+        print(f'received data:{team_name}')
+
+        teams[team_name] = conn
+
+        # assign team to a group
+        if group_index % 2 == 0:
+            group2.append(team_name)
+        else:
+            group1.append(team_name)
+        conn.send(b'Welcome to Keyboard Spamming Battle Royale.')
+        group_index += 1
+
+def quit(sig, frame):
+    print('Goosbye!')
+    sys.exit(0)
 
 
 def main():
+    signal.signal(signal.SIGINT, quit)
 
     # TODO: add option for eth2
     IP = get_if_addr("eth1")
@@ -66,7 +134,7 @@ def main():
     while True:
         thread = Thread(target = send_offer, args=[IP])
         thread.start()
-        game = listen_for_clients(IP)
+        game = play(IP)
         thread.join()
         if game:
             print('Game over, sending out offer requests...' )
